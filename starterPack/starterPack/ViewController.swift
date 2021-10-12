b18e711882f92e693544875232711973c5276f0f
maggielucas <milucas@uvm.edu>
2021-10-12 11:43:01 -0400

    messing with storyboard

--- starterPack/starterPack/ViewController.swift
+++ starterPack/starterPack/ViewController.swift
@@ -4,6 +4,7 @@
 //
 //  Created by Joseph Wills on 10/12/21.
 //
+// Main Menu screen
 
 import UIKit
 
@@ -11,8 +12,21 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        welcome.text = "WELCOME TO STARTERPACK"
     }
+    
+    //initialize label and buttons
+    @IBOutlet var welcome: UILabel!
+    @IBOutlet var temperatureButton: UIButton!
+    @IBOutlet var currencyButton: UIButton!
+    @IBOutlet var measureButton: UIButton!
+    @IBOutlet var timeButton: UIButton!
+    @IBOutlet var trafficButton: UIButton!
+    @IBOutlet var clothingButton: UIButton!
+    @IBOutlet var phrasesButton: UIButton!
+    @IBOutlet var translateButton: UIButton!
+    @IBOutlet var emergencyButton: UIButton!
+
 
 
 }
