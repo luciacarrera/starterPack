a0d75e2c5cce51c202bbc3a99378994ed779c241
jmwills <44883558+jmwills@users.noreply.github.com>
2021-10-12 10:29:07 -0400

    Add files via upload

---
+++ starterPack/starterPack/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  starterPack
+//
+//  Created by Joseph Wills on 10/12/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
