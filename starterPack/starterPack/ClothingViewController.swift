802b6ac3932172c0785b85cb58e2ccc7c78d4358
maggielucas <milucas@uvm.edu>
2021-10-20 15:15:32 -0400

    set up clothing view (kinda)

--- starterPack/starterPack/ClothingViewController.swift
+++ starterPack/starterPack/ClothingViewController.swift
@@ -15,15 +15,13 @@ class ClothingViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    // establish variables
+    @IBOutlet  var shoeSizeSelector: [UIMenu]!
+    @IBOutlet var pantsSizeSelector: [UIMenu]!
+    @IBOutlet var shirtSizeSelector: [UIMenu]!
+    @IBOutlet var USShoeSize: UILabel!
+    @IBOutlet var USPantSize: UILabel!
+    @IBOutlet var USShirtSize: UILabel!
+    
+    
 }
