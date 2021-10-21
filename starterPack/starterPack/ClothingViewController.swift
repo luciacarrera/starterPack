e686eab74220d48058c76199c53c3dfafc649994
maggielucas <milucas@uvm.edu>
2021-10-21 16:49:31 -0400

    clothing size converter storyboard!! yay

--- starterPack/starterPack/ClothingViewController.swift
+++ starterPack/starterPack/ClothingViewController.swift
@@ -9,19 +9,64 @@ import UIKit
 
 class ClothingViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        print("clothing view did load")
         // Do any additional setup after loading the view.
+        shoeSizePicker.dataSource = self
+        shoeSizePicker.delegate = self
+        
+        pantsSizePicker.dataSource = self
+        pantsSizePicker.delegate = self
+        
+        shirtSizePicker.dataSource = self
+        shirtSizePicker.delegate = self
     }
     
     // establish variables
-    @IBOutlet  var shoeSizeSelector: [UIMenu]!
-    @IBOutlet var pantsSizeSelector: [UIMenu]!
-    @IBOutlet var shirtSizeSelector: [UIMenu]!
-    @IBOutlet var USShoeSize: UILabel!
-    @IBOutlet var USPantSize: UILabel!
-    @IBOutlet var USShirtSize: UILabel!
+    @IBOutlet var shoeSizePicker: UIPickerView!
+    let shoeSizes = [5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10]
     
     
+    @IBOutlet var pantsSizePicker: UIPickerView!
+    let pantSizes = [26, 28, 30, 32, 34]
+    
+    @IBOutlet var shirtSizePicker: UIPickerView!
+    let shirtSizes = ["small", "medium", "large", "xtra large"]
+
+}
+
+
+extension ClothingViewController: UIPickerViewDataSource {
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if pickerView.tag == 1 {
+            return 10
+        }
+        else if pickerView.tag == 2 {
+            return 5
+        }
+        else if pickerView.tag == 3 {
+            return 4
+        }
+        return 0
+    }
+
+}
+extension ClothingViewController: UIPickerViewDelegate {
+    public func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if pickerView.tag == 1 {
+            return String(shoeSizes[row])
+        }
+        else if pickerView.tag == 2 {
+            return String(pantSizes[row])
+        }
+        else if pickerView.tag == 3 {
+            return shirtSizes[row]
+        }
+        return "???"
+    }
 }
