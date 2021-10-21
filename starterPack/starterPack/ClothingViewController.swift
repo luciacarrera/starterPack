8e66164b989ecd9fe4cb2230ba11a715ab2ad938
maggielucas <milucas@uvm.edu>
2021-10-21 17:27:47 -0400

    picker changes label :)

--- starterPack/starterPack/ClothingViewController.swift
+++ starterPack/starterPack/ClothingViewController.swift
@@ -27,14 +27,19 @@ class ClothingViewController: UIViewController {
     // establish variables
     @IBOutlet var shoeSizePicker: UIPickerView!
     let shoeSizes = [5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10]
+    @IBOutlet var USShoe: UILabel!
+    
     
     
     @IBOutlet var pantsSizePicker: UIPickerView!
     let pantSizes = [26, 28, 30, 32, 34]
+    @IBOutlet var USpant: UILabel!
+    
     
     @IBOutlet var shirtSizePicker: UIPickerView!
     let shirtSizes = ["small", "medium", "large", "xtra large"]
-
+    @IBOutlet var USShirt: UILabel!
+    
 }
 
 
@@ -53,6 +58,18 @@ extension ClothingViewController: UIPickerViewDataSource {
             return 4
         }
         return 0
+    }
+    
+    public func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if pickerView.tag == 1 {
+            USShoe.text = String(shoeSizes[row])
+        }
+        else if pickerView.tag == 2 {
+            USpant.text = String(pantSizes[row])
+        }
+        else if pickerView.tag == 3 {
+            USShirt.text = shirtSizes[row]
+        }
     }
 
 }
