a0f898cb7f70150c82467b540c51bfdb28cc521d
Lucía Carrera <61793180+luciacarrera@users.noreply.github.com>
2021-10-21 17:51:16 -0400

    TimeView almost finished - properly uploaded

--- starterPack/starterPack/ClothingViewController.swift
+++ starterPack/starterPack/ClothingViewController.swift
@@ -9,87 +9,19 @@ import UIKit
 
 class ClothingViewController: UIViewController {
 
-    // TODO: change picker values based on country of origin
-    // TODO: change converter based on database
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("clothing view did load")
+
         // Do any additional setup after loading the view.
-        shoeSizePicker.dataSource = self
-        shoeSizePicker.delegate = self
-        
-        pantsSizePicker.dataSource = self
-        pantsSizePicker.delegate = self
-        
-        shirtSizePicker.dataSource = self
-        shirtSizePicker.delegate = self
     }
     
     // establish variables
-    @IBOutlet var shoeSizePicker: UIPickerView!
-    let shoeSizes = [5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10]
-    @IBOutlet var USShoe: UILabel!
+    @IBOutlet  var shoeSizeSelector: [UIMenu]!
+    @IBOutlet var pantsSizeSelector: [UIMenu]!
+    @IBOutlet var shirtSizeSelector: [UIMenu]!
+    @IBOutlet var USShoeSize: UILabel!
+    @IBOutlet var USPantSize: UILabel!
+    @IBOutlet var USShirtSize: UILabel!
     
-    var shoeConversion = [5.5:7.5, 6:8, 6.5:8.5, 7:9, 7.5:9.5, 8:10, 8.5:10.5, 9:11, 9.5:11.5, 10:12]
     
-    
-    @IBOutlet var pantsSizePicker: UIPickerView!
-    let pantSizes = [34, 36, 38, 40, 42, 44, 46, 48, 50]
-    @IBOutlet var USpant: UILabel!
-    var pantConversion = [34:2, 36:4, 38:6, 40:8, 42:10, 44:12, 46:14, 48:16, 50:18]
-    
-    @IBOutlet var shirtSizePicker: UIPickerView!
-    let shirtSizes = ["small", "medium", "large", "xtra large", "2XL"]
-    @IBOutlet var USShirt: UILabel!
-    
-    // this is just random nonsense, i promise its not a dig at americans being big ;)
-    var shirtConversion = ["small":"xtra small", "medium":"small", "large":"medium", "xtra large":"large", "2XL": "xtra large"]
-    
-}
-
-
-extension ClothingViewController: UIPickerViewDataSource {
-    public func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        if pickerView.tag == 1 {
-            return 10
-        }
-        else if pickerView.tag == 2 {
-            return 5
-        }
-        else if pickerView.tag == 3 {
-            return 5
-        }
-        return 0
-    }
-    
-    public func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        if pickerView.tag == 1 {
-            USShoe.text = String(shoeConversion[shoeSizes[row]]!)
-        }
-        else if pickerView.tag == 2 {
-            USpant.text = String(pantConversion[pantSizes[row]]!)
-        }
-        else if pickerView.tag == 3 {
-            USShirt.text = shirtConversion[shirtSizes[row]]!
-        }
-    }
-
-}
-extension ClothingViewController: UIPickerViewDelegate {
-    public func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        if pickerView.tag == 1 {
-            return String(shoeSizes[row])
-        }
-        else if pickerView.tag == 2 {
-            return String(pantSizes[row])
-        }
-        else if pickerView.tag == 3 {
-            return shirtSizes[row]
-        }
-        return "???"
-    }
 }
--- starterPack/starterPack/TimeViewController.swift
+++ starterPack/starterPack/TimeViewController.swift
@@ -8,11 +8,73 @@
 import UIKit
 
 class TimeViewController: UIViewController {
+    //labels - home country
+    @IBOutlet var homeTimeLabel: UILabel!
+    @IBOutlet var homeTime: UILabel!
+    @IBOutlet var homeDate: UILabel!
 
+    //labels - visiting country
+    @IBOutlet var visitingTimeLabel: UILabel!
+    @IBOutlet var visitingTime: UILabel!
+    @IBOutlet var visitingDate: UILabel!
+    
+    // labels - time difference
+    @IBOutlet var timeDifference: UILabel!
+
+    //buttons
+    @IBOutlet var exit: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        /* CONSTANTS */
+        // the following constant should become a global constant in next versions
+        let visitingCity = "New York"
+        let homeCity = "Madrid"
+        let homeTimeZone = "CEST"
+        
+        /* TEXT LABELS */
+        visitingTimeLabel.text = "In "+visitingCity + ", it is"
+        homeTimeLabel.text = "In "+homeCity + ", it is"
+        
+        /* VISITING TIME AND DATE LABELS */
+        // Initialization of swift date, calendar and dateformatter objects
+        let date = Date()
+        let timeFormatter = DateFormatter()
+        let dateFormatter = DateFormatter()
 
-        // Do any additional setup after loading the view.
+        // Formatting of time to appear
+        timeFormatter.dateFormat = "HH:mm a"
+        dateFormatter.dateFormat = "EEEE, LLLL dd"
+
+        // text to appear on screen
+        let vTime = timeFormatter.string(from: date).lowercased()
+        let vDate = dateFormatter.string(from: date)
+        visitingTime.text = vTime
+        visitingDate.text = vDate
+
+        
+        /* HOME TIME AND DATE LABELS */
+        timeFormatter.timeZone = TimeZone(abbreviation: homeTimeZone)
+        let hTime = timeFormatter.string(from: date).lowercased()
+        let hDate = dateFormatter.string(from: date)
+        homeTime.text = hTime
+        homeDate.text = hDate
+
+        /* DIFFERENCE LABEL */
+        // getting hours to do the math
+        let hHour = Int(hTime.prefix(2))
+        let vHour = Int(vTime.prefix(2))
+        
+        /* math part
+        let diff = hHour - vHour
+        var diffStr = ""
+        if diff
+        
+        timeDifference.text = homeCity + " is "
+        
+       Initialization of home countries time
+         */ 
     }
     
 
