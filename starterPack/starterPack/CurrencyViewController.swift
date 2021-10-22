d49573bb0722eb21e121796aa6e2ce88b2f0e42d
efaherty <efaherty@uvm.edu>
2021-10-22 18:56:13 -0400

    currency converter working decently - need to change formatting and fix euro -> dollar

--- starterPack/starterPack/CurrencyViewController.swift
+++ starterPack/starterPack/CurrencyViewController.swift
@@ -8,22 +8,69 @@
 import UIKit
 
 class CurrencyViewController: UIViewController {
+    
+    @IBOutlet var euroTextField: UITextField!
+    @IBOutlet var dollarTextField: UITextField!
+    var dollarValue: Measurement<Unit>? {
+        didSet {
+            updateEuroText()
+        }
+    }
+    var euroValue: Decimal? {
+        didSet {
+            updateDollarText()
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        updateDollarText()
+        updateEuroText()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func dollarFieldEditingChanged(_ textField: UITextField) {
+        if let text = textField.text, let value = Double(text) {
+            dollarTextField.text = String(value * 1.164)
+        } else {
+            dollarValue = nil
+        }
     }
-    */
+    
+    @IBAction func euroFieldEditingChanged(_ textField: UITextField) {
+        if let text = textField.text, let value = Double(text){
+            euroTextField.text = String(value * 0.859)
+        } else {
+            euroTextField.text = "???"
+        }
+    }
+    
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        dollarTextField.resignFirstResponder()
+        euroTextField.resignFirstResponder()
+    }
+    
+    func updateEuroText() {
+        if let euroValue = euroValue {
+            euroTextField.text = "\(euroValue)"
+        } else {
+            euroTextField.text = "???"
+        }
+    }
+    
+    func updateDollarText() {
+        if let dollarValue = dollarValue {
+            dollarTextField.text = "\(dollarValue)"
+        } else {
+            dollarTextField.text = "???"
+        }
+    }
+    
+//    let numberFormatter: NumberFormatter = {
+//        let nf = NumberFormatter()
+//        nf.numberStyle = .decimal
+//        nf.minimumFractionDigits = 0
+//        nf.maximumFractionDigits = 1
+//        return nf
+//    }
 
 }
