d6aa8d82a1045def977c048145fd50147861237f
efaherty <efaherty@uvm.edu>
2021-11-18 11:18:34 -0500

    translation page is functional :)

--- starterPack/starterPack/TranslateViewController.swift
+++ starterPack/starterPack/TranslateViewController.swift
@@ -10,17 +10,21 @@ import LanguageTranslatorV3
 
 class TranslateViewController: UIViewController {
     
-    @IBOutlet var inputTextField: UITextField!
+    @IBOutlet var inputTextView: UITextView!
     
     @IBOutlet var translateButton: UIButton!
     
-    @IBOutlet var transLabel: UILabel!
+    @IBOutlet var transView: UITextView!
     
+    @IBOutlet var fromLabel: UILabel!
     
+    @IBOutlet var toLabel: UILabel!
+    
+    var fromLanguage = "Spanish"
+    
+    var toLanguage = "English"
     
     let transLanguages = ["Arabic":"ar", "Bengali":"bn", "Bosnian":"bs", "Bulgarian":"bg", "Chinese (Simplified)":"zh", "Chinese (Traditional)":"zh-TW", "Croatian":"hr", "Czech":"cs", "Danish":"da", "Dutch":"nl", "English":"en", "Estonian":"et", "Finnish":"fi", "French":"fr", "German":"de", "Greek":"el", "Gujarati":"gu", "Hebrew":"he", "Hindi":"hi", "Hungarian":"hu", "Irish":"ga", "Indonesian":"id", "Italian":"it", "Japanese":"ja", "Korean":"ko", "Latvian":"lv", "Lithuanian":"lt", "Malay":"ms", "Malayalam":"ml", "Maltese":"mt", "Nepali":"ne", "Norwegian Bokmål":"nb", "Polish":"pl", "Portuguese":"pt", "Romanian":"ro", "Russian":"ru", "Sinhala":"si", "Slovak":"sk", "Slovenian":"sl", "Spanish":"es", "Swedish":"sv", "Tamil":"ta", "Telugu":"te", "Thai":"th", "Turkish":"tr", "Ukrainian":"uk", "Urdu":"ur", "Vietnamese":"vi", "Welsh":"cy"]
-    var fromLanguage = "English"
-    var toLanguage = "Spanish"
 
     func translate(to: String, from: String) -> String? {
         var output = "placeholder"
@@ -30,9 +34,7 @@ class TranslateViewController: UIViewController {
         languageTranslator.serviceURL = "https://api.au-syd.language-translator.watson.cloud.ibm.com/instances/09b1d916-b5e9-4b06-8d20-f0d58800b7cd"
 
         let input = getInput()!
-        
 
-        print("1translator function: ", output)
         languageTranslator.translate(text: [input], modelID: transModel) {
             response, error -> Void in
 
@@ -42,37 +44,43 @@ class TranslateViewController: UIViewController {
             }
 
             output = translation.translations[0].translation
-            print("2translator function: ", output)
         }
+        sleep(3)
         return output
     }
     
     @IBAction func translateInput() {
-//        _ = translate(to: toLanguage, from: fromLanguage)
         let output = translate(to: toLanguage, from: fromLanguage)
-//        let output = "test"
-//        transLabel.text = output
-        print("translateInput: ", output!)
         updateTranslation(to: output!)
         
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateLabels()
+    }
+    
+    func updateLabels() {
+        fromLabel.text = fromLanguage
+        toLabel.text = toLanguage
+    }
+    
+    @IBAction func swapLanguages() {
+        fromLanguage = "English"
+        toLanguage = "Spanish"
     }
     
     func getInput() -> String? {
-        let input = inputTextField.text
+        let input = inputTextView.text
         return input
     }
     
     @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
-        inputTextField.resignFirstResponder()
+        inputTextView.resignFirstResponder()
     }
     
     func updateTranslation(to: String) {
-        transLabel.self.text = to
-        print("updateTranslation: ", to)
+        transView.self.text = to
     }
 
 
