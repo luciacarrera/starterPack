5751cb997661bab624386534ee93a9e750831c4d
maggielucas <milucas@uvm.edu>
2021-12-06 15:43:07 -0500

    expand pickers/clean user page

--- starterPack/starterPack/TimeViewController.swift
+++ starterPack/starterPack/TimeViewController.swift
@@ -101,18 +101,5 @@ class TimeViewController: UIViewController {
         }
         
         timeDifference.text = timeDifferenceString
-        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
